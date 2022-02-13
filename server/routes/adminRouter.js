30850b1174425cd71a544c5d025d67fd4af04489
gutierya <52222018+gutierya@users.noreply.github.com>
2022-02-12 19:17:25 -0500

    added debug backend dependancy; organized index.js routes section; created adminRouter.js file and started draft

---
+++ server/routes/adminRouter.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+// TODO: const adminController = require('../controllers/admin-controller');
+
+const adminRouter = express.Router();
+
+adminRouter.route('/admin').get((req, res) => {
+  const url = 'mongodb+srv://crtl123:crtl123@crtlaltelitecluster.1bbx9.mongodb.net/PatientData';
+  const dbName = 'PatientData';
+
+  (async function mongo() {
+    let client;
+    try {
+      client = await MongoClient.connect(url);
+      debug('Connected correctly to database server');
+
+      const db = client.db(dbName);
+
+      const response = await db
+        .collection('PatientData')
+        .find()
+        .toArray();
+
+      res.send(response);
+    } catch (err) {
+      debug(err.stack);
+    }
+    client.close();
+  })();
+});
