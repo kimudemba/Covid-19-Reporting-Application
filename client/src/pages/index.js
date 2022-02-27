96a8baa412e4daf2c854967c4303671fb43a807c
gutierya <52222018+gutierya@users.noreply.github.com>
2022-02-27 15:11:33 -0500

    adding pages/ExamTable class file to the index.js file inside of pages for routing views on app.js

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,18 +2,20 @@ import PatientInsert from './PatientInsert';
 import Items from './Items';
 import PatientList from './PatientList';
 import ItemsPlain from './ItemsPlain';
-// import ItemsTable from './ItemsTable'; 
+// import ItemsTable from './ItemsTable';
 //import ItemUpdate from './ItemUpdate';
 import PatientTable from './PatientTable';
 import PatientUpdate from './PatientUpdate';
+import ExamTable from './ExamTable';
 
 export {
-    Items,
-    PatientList,
-    ItemsPlain,
-   // ItemsTable,
-   // ItemUpdate,
-    PatientTable,
-    PatientUpdate,
-    PatientInsert,
+  Items,
+  PatientList,
+  ItemsPlain,
+  // ItemsTable,
+  // ItemUpdate,
+  PatientTable,
+  PatientUpdate,
+  PatientInsert,
+  ExamTable,
 };
