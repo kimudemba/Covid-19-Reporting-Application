d4030ac634019079c0bea90df424371313c62a48
gutierya <52222018+gutierya@users.noreply.github.com>
2022-03-10 16:49:39 -0500

    cleaned up comments in backend

--- server/routes/patientsRouter.js
+++ server/routes/patientsRouter.js
@@ -1,23 +1,18 @@
 /**
  * Patients Router
  **/
-
 const express = require('express');
 const debug = require('debug')('index: examsRouter');
 const patientsController = require('../controllers/patientsController');
-const { MongoClient } = require('mongodb');
-
 const patientsRouter = express.Router();
 
 // '/' === '/api/patients'
 // http://localhost:3000/api/patients/ - inside server.js this uses stmt: app.use('/api/patients', patientsRouter) - to get all patients
 patientsRouter.get('/', patientsController.getPatients);
-
 patientsRouter.post('/', patientsController.createOnePatient);
 
 // api/patients/:id
 patientsRouter.get('/:PATIENT_ID', patientsController.getpatientById);
-
 patientsRouter.put('/:PATIENT_ID', patientsController.updatePatient);
 
 patientsRouter.delete('/patients', patientsController.deleteOnePatient);
